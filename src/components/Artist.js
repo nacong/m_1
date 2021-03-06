9455b971cc86d6d3cc2e6bd07c2383b7a1032e23
quincy <50263129+insta0430@users.noreply.github.com>
2021-03-06 16:46:49 +0900

    css update
    이제 그림자 값이 CSS와 동일합니다.
    FHD 16:9 기준 포토샵과 거의 동일하게 표시됩니다.
    
     - FHD 16:9가 아니라면 정상적으로 표시되지 않거나 이상하게 보일 수 있습니다

--- src/components/Artist.js
+++ src/components/Artist.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => (
     <div className="artist">
-        <b>'디자이너'</b><b id="A1">님의 작품</b>
+        <div>'디자이너'<div id="A1">님의 작품</div></div>
     </div>
 )
 
