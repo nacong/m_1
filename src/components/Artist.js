1d85a05f56c5b17c5534f41f9290baf7d08ac489
quincy <50263129+insta0430@users.noreply.github.com>
2021-03-07 02:06:32 +0900

    justify-content
    이제 '디자이너'님의 작품은 한줄에 표시됩니다.
    여백이 40px 이상에서 화면을 채울정도로 커집니다.

--- src/components/Artist.js
+++ src/components/Artist.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => (
     <div className="artist">
-        <div>'디자이너'<div id="A1">님의 작품</div></div>
+        <span>'디자이너'<span id="A1">님의 작품</span></span>
     </div>
 )
 
