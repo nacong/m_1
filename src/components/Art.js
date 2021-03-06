531a2655101c682d5c6705fcab167d40b6f0f656
quincy <50263129+insta0430@users.noreply.github.com>
2021-03-06 18:11:37 +0900

    ...
    넘치는 글자에 ...을 적용합니다

--- src/components/Art.js
+++ src/components/Art.js
@@ -1,42 +1,75 @@
 import React from 'react';
 
 const Art = () => (
-    <div className="art">
-        <div id="df">
-            <p id="title">제목입니다열글자글자</p>
-            <img alt="" id="img" src="https://images.unsplash.com/photo-1604922824961-87cefb2e4b07?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8MSUzQTF8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60"></img>
+    <div class="art-container">
+        <div id="art-box">
+            <p id="title">제목입니다 엄청 긴 글자일거에요</p>
+            <div id="img-padding">
+                <img alt="" id="img" src="https://source.unsplash.com/512x512/?graphic"></img>
+            </div>
         </div>
-        <div id="df">
+
+        <div id="art-box">
             <p id="title">제목입니다열글자글자</p>
-            <img alt="" id="img" src="https://images.unsplash.com/photo-1604922824961-87cefb2e4b07?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8MSUzQTF8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60"></img>
+            <div id="img-padding">
+                <img alt="" id="img" src="https://source.unsplash.com/512x512/?graphic"></img>
+            </div>
         </div>
-        <div id="df">
+
+        <div id="art-box">
             <p id="title">제목입니다열글자글자</p>
-            <img alt="" id="img" src="https://images.unsplash.com/photo-1604922824961-87cefb2e4b07?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8MSUzQTF8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60"></img>
+            <div id="img-padding">
+                <img alt="" id="img" src="https://source.unsplash.com/512x512/?graphic"></img>
+            </div>
         </div>
-        <div id="df">
+
+        <div id="art-box">
             <p id="title">제목입니다열글자글자</p>
-            <img alt="" id="img" src="https://images.unsplash.com/photo-1604922824961-87cefb2e4b07?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8MSUzQTF8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60"></img>
+            <div id="img-padding">
+                <img alt="" id="img" src="https://source.unsplash.com/512x512/?graphic"></img>
+            </div>
         </div>
-        <div id="df">
+
+        <div id="art-box">
             <p id="title">제목입니다열글자글자</p>
-            <img alt="" id="img" src="https://images.unsplash.com/photo-1604922824961-87cefb2e4b07?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8MSUzQTF8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60"></img>
+            <div id="img-padding">
+                <img alt="" id="img" src="https://source.unsplash.com/512x512/?graphic"></img>
+            </div>
         </div>
-        <div id="df">
+
+        <div id="art-box">
             <p id="title">제목입니다열글자글자</p>
-            <img alt="" id="img" src="https://images.unsplash.com/photo-1604922824961-87cefb2e4b07?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8MSUzQTF8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60"></img>
+            <div id="img-padding">
+                <img alt="" id="img" src="https://source.unsplash.com/512x512/?graphic"></img>
+            </div>
         </div>
-        <div id="df">
+
+        <div id="art-box">
             <p id="title">제목입니다열글자글자</p>
-            <img alt="" id="img" src="https://images.unsplash.com/photo-1604922824961-87cefb2e4b07?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8MSUzQTF8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60"></img>
+            <div id="img-padding">
+                <img alt="" id="img" src="https://source.unsplash.com/512x512/?graphic"></img>
+            </div>
         </div>
-        <div id="df">
+
+        <div id="art-box">
             <p id="title">제목입니다열글자글자</p>
-            <img alt="" id="img" src="https://images.unsplash.com/photo-1604922824961-87cefb2e4b07?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8MSUzQTF8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60"></img>
+            <div id="img-padding">
+                <img alt="" id="img" src="https://source.unsplash.com/512x512/?graphic"></img>
+            </div>
         </div>
-        <div id="df">
+
+        <div id="art-box">
             <p id="title">제목입니다열글자글자</p>
-            <img alt="" id="img" src="https://images.unsplash.com/photo-1604922824961-87cefb2e4b07?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8MSUzQTF8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60"></img>
+            <div id="img-padding">
+                <img alt="" id="img" src="https://source.unsplash.com/512x512/?graphic"></img>
+            </div>
+        </div>
+
+        <div id="art-box">
+            <p id="title">제목입니다열글자글자</p>
+            <div id="img-padding">
+                <img alt="" id="img" src="https://source.unsplash.com/512x512/?graphic"></img>
+            </div>
         </div>
     </div>
 )
